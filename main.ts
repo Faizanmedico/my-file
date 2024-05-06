3b04f2967630437bf79fa855f79bbf31088b7666
faizanmedico <faizanmedico@gmail.com>
2024-05-07 03:05:44 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,38 @@
+
+
+
+for (let i = 0; i < 2; i++) {
+    console.log ("Hello." + i);
+  }
+
+
+  for(let a = 1; a<=2; a++) {
+    console.log("Pakistan Zindabad " +a);
+    console.log("sultan");
+
+  }
+
+
+
+  let arr = [10, 20, 30, 40];
+
+for (var index1 in arr) {
+  console.log(index1); // prints indexes: 0, 1, 2, 3
+}
+
+let i: number = 5;
+do {
+    console.log("Block statement execution no." + i )
+    i++;
+} while ( i < 10)
+
+
+
+
+    //^^^^^^^^
+
+
+    for (let i = 0; i <= 9; i++) {
+      console.log (1 + i)
+    }
+    
