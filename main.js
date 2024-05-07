9a6ee8b5d384d23a11139c5baacb0049c1ec3d72
faizanmedico <faizanmedico@gmail.com>
2024-05-07 12:05:29 +0500

    Adding my two files

--- main.js
+++ main.js
@@ -18,4 +18,36 @@ do {
 for (let i = 0; i <= 9; i++) {
     console.log(1 + i);
 }
+function disp_details(id, name, mail_id) {
+    console.log("ID:", id);
+    console.log("Name", name);
+    if (mail_id != undefined)
+        console.log("Email Id", mail_id);
+}
+disp_details(123, "John");
+disp_details(111, "mary", "mary@xyz.com");
+//******* 
+function addNumbers(...nums) {
+    var i;
+    var sum = 0;
+    for (i = 0; i < nums.length; i++) {
+        sum = sum + nums[i];
+    }
+    console.log("sum of the numbers", sum);
+}
+addNumbers(1, 2, 3);
+addNumbers(10, 10, 10, 10, 10);
+//  ******
+function calculate_discount(price, rate = 0.50) {
+    var discount = price * rate;
+    console.log("Discount Amount: ", discount);
+}
+calculate_discount(1000);
+calculate_discount(1000, 0.30);
+function disp(x, y) {
+    console.log(x);
+    console.log(y);
+}
+disp("abc");
+disp(1, "xyz");
 export {};
